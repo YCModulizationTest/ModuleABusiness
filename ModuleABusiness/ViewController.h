580275513266180e86ef61c6570c9e5dfe04a029
wlcui <Alfred03@126.com>
2018-09-27 11:29:53 +0800

    添加组件A项目

---
+++ ModuleABusiness/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleABusiness
+//
+//  Created by Alfred Tsui on 2018/9/27.
+//  Copyright © 2018年 WENLONG Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModuleABusiness/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ModuleABusiness
+//
+//  Created by Alfred Tsui on 2018/9/27.
+//  Copyright © 2018年 WENLONG Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
